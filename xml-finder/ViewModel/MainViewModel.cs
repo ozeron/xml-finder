af7505c671d39aa2f966969e015b43ecdb9e7c7e
Alex Lapchenko <ozeron@me.com>
2013-12-21 00:32:49 +0200

    Created MainViewModel class and bint it to te MainWindow.

---
+++ xml-finder/ViewModel/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using xml_finder.Model;
+
+namespace xml_finder.ViewModel
+{
+    class MainViewModel //: ViewModelBase
+    {
+        private List<Track> _tracks;
+
+        public List<Track> Tracks
+        {
+            get
+            {
+                return _tracks;
+            }
+            set
+            {
+                _tracks = value;
+                RaisePropertyChanged("Tracks");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
